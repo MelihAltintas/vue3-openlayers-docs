0b7108395533bd3afae516657fafc3fddb5ba323
MelihAltintas <melihaltintas@hotmail.com>
2021-06-28 11:51:05 +0300

    sitemap add

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -127,7 +127,7 @@ module.exports = {
                         text: 'Map Controls',
                         link: '/componentsguide/mapcontrols/'
 
-                    }
+                    }       
                 ]
             }, ]
 
---
+++ docs/.vitepress/dist/assets/marker.843eb974.js
@@ -0,0 +1,1 @@
+var a="/assets/marker.8ab6992c.png";export{a as m};
