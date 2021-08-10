e04e4f8b9413ad89d0102afeae0e65108416268c
MelihAltintas <melihaltintas@hotmail.com>
2021-08-10 12:21:54 +0300

    fontawesome css

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import 'vue3-openlayers/dist/vue3-openlayers.css'
-import 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css'
+import '@fortawesome/fontawesome-free/css/all.css'
 createApp(App).mount('#app')
