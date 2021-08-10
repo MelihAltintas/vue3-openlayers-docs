2b294f7c7c4fc0809bd9da9af26ba90fb6b8c35c
MelihAltintas <melihaltintas@hotmail.com>
2021-08-10 12:38:49 +0300

    fontawesome css

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -2,7 +2,7 @@ module.exports = {
     title: 'Vue3 OpenLayers',
     description: 'Vue3 OpenLayers',
     head: [
-        ['link', {src: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css'}]
+        ['link', {rel: "stylesheet",href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css'}]
     ],
     themeConfig: {
         repo: 'MelihAltintas/vue3-openlayers',
